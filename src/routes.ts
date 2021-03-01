78a2654ce4254f23d8f40d11937d28a35e505da2
Henrique Balmant <henrique.balmant@gmail.com>
2021-02-28 21:08:04 -0300

    Add surveys show route

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,16 @@ const router = Router();
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 
+// Users routes
+
+// Create users
 router.post('/users', usersController.create);
+
+// Surveys routes
+
+// Create surveys
 router.post('/surveys', surveysController.create);
+// Show surveys
+router.get('/surveys', surveysController.show);
 
 export { router };
