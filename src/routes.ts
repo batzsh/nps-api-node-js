ccaf720066f26df4021da68be3611744df583c6a
Henrique Balmant <henrique.balmant@gmail.com>
2021-02-28 21:04:32 -0300

    Add surveys controller and create route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { UsersController } from './controllers/UsersController';
+import { SurveysController } from './controllers/SurveysController';
 
 const router = Router();
 
 const usersController = new UsersController();
+const surveysController = new SurveysController();
 
 router.post('/users', usersController.create);
+router.post('/surveys', surveysController.create);
 
 export { router };
