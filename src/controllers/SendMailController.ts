eeb6a647fde73799ce5367fd02d53fe4f3878ba9
Henrique Balmant <henrique.balmant@gmail.com>
2021-03-12 18:04:45 -0300

    Add table relatiosn to SurveyUser

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -43,6 +43,7 @@ class SendMailController {
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
       where: [{ user_id: user.id }, { value: null }],
+      relations: ['user', 'survey'],
     });
 
     if (surveyUserAlreadyExists) {
