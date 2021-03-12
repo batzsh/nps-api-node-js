5b95d828a14ee26cbf7386ece2d666fb0ae0d06d
Henrique Balmant <henrique.balmant@gmail.com>
2021-03-12 12:51:03 -0300

    Add email template

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -4,6 +4,7 @@ import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UsersRepository';
 import SendMailService from '../services/SendMailService';
+import { resolve } from 'path';
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -13,9 +14,9 @@ class SendMailController {
     const surveysRepository = getCustomRepository(SurveysRepository);
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-    const userAlreadyExists = await usersRepository.findOne({ email });
+    const user = await usersRepository.findOne({ email });
 
-    if (!userAlreadyExists) {
+    if (!user) {
       return response.status(400).json({
         error: 'User does not exists.',
       });
@@ -32,13 +33,20 @@ class SendMailController {
     }
 
     const surveyUser = surveysUsersRepository.create({
-      user_id: userAlreadyExists.id,
+      user_id: user.id,
       survey_id,
     });
 
     await surveysUsersRepository.save(surveyUser);
 
-    await SendMailService.execute(email, survey.title, survey.description);
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+    };
+
+    await SendMailService.execute(email, survey.title, variables, npsPath);
 
     return response.json(surveyUser);
   }
